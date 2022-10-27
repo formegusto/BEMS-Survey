88cd535965c0058965bfae4c255b7d4766fd860b
formegusto <formegusto@gmail.com>
2022-10-27 15:24:03 +0900

    mobile test okay, required not scroll;;

--- bs-fe/src/components/success/index.tsx
+++ bs-fe/src/components/success/index.tsx
@@ -6,7 +6,7 @@ function SuccessComponent() {
   return (
     <Wrap>
       <BackgroundImg
-        src="http://localhost:8080/static/building/building-1656563478294.jpeg"
+        src={`${process.env.REACT_APP_BACK_END_API_SERVER}/static/building/building-1656563478294.jpeg`}
         alt="building-img"
       />
       <Block>
--- bs-fe/src/containers/detail/index.tsx
+++ bs-fe/src/containers/detail/index.tsx
@@ -2,16 +2,22 @@ import { DetailComponent } from "@components";
 import { FDetailInfo } from "@store/types";
 import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 function DetailContainer() {
+  const navigate = useNavigate();
   const { register, handleSubmit, setValue, watch } = useForm<FDetailInfo>({});
 
   const rank1Watch = watch("1rank");
   const rank2Watch = watch("2rank");
 
-  const onSubmit: SubmitHandler<FDetailInfo> = React.useCallback((data) => {
-    console.log(data);
-  }, []);
+  const onSubmit: SubmitHandler<FDetailInfo> = React.useCallback(
+    (data) => {
+      console.log(data);
+      navigate("/success");
+    },
+    [navigate]
+  );
 
   return (
     <DetailComponent
